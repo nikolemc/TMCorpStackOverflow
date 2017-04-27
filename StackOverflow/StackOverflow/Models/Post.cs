508157c55ced2db2b73baa4fa92cb3fdaf0c8ca7
datctran <datc.tran@gmail.com>
2017-04-27 17:11:23 -0400

    Revert "Revert "post comment vote added to models""

---
+++ StackOverflow/StackOverflow/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflow.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PostContent { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime PostedTimeStamp { get; set; }
+        public ICollection<PostVote> Votes { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+
+
+    }
+}
