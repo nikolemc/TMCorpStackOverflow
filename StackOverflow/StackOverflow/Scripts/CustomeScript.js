d2840cc8a24b062775fff1135668835d8f2048c7
Dat Tran <datc.tran@gmail.com>
2017-04-28 14:06:22 -0400

    needed to add customscript file

---
+++ StackOverflow/StackOverflow/Scripts/CustomeScript.js
@@ -0,0 +1,51 @@
+﻿let upVote = (commentId) => {
+    let _data = {
+        id: commentId,
+        voteValue: 1
+    };
+
+    if (!commentId) {
+        alert("No Post Found");
+    }
+    else {
+        $.ajax({
+            url: "/CommentVotes/Vote",
+            data: JSON.stringify(_data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "html",
+            success: (newHtml) => {
+                $("#VoteCountDisplay-" + commentId).html(newHtml);
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+
+            }
+        })
+    }
+}
+
+let downVote = (commentId) => {
+    let _data = {
+        id: commentId,
+        voteValue: -1
+    }
+
+    if (!commentId) {
+        alert("No Post Found");
+    }
+    else {
+        $.ajax({
+            url: "/CommentVotes/Vote",
+            data: JSON.stringify(_data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "html",
+            success: (newHtml) => {
+                $("#VoteCountDisplay-" + commentId).html(newHtml);
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+
+            }
+        })
+    }
+}
